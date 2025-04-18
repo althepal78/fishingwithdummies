2526f43ab50d9a5c10a615b5846813f7f32a8c17
althepal78 <althepal78@gmail.com>
2025-04-18 16:32:09 -0400

    oh snap a lot changed. I added a contact section with icons, then I added it to the navigation bar as a link to the footer.

--- index.php
+++ index.php
@@ -1,118 +1,158 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta
-      name="description"
-      content="As Captain Al, I'm getting back into fishing after many years away. I learned to fish with my father as a child, but it's been a long time since then. I'm starting over, picking up tips from various online sources, though none are specific to Spring Hill, Florida. I plan to fish around Clearwater, possibly St. Petersburg, and Tampa. My boat is an old cruiser, not built for fishing, so I'm hoping it won't cause any issues. I'll have company on board and will be trying out online fishing techniques, though they might not always work perfectly for the local waters."
-    />
-    <meta
-      name="keywords"
-      content="#FishingLife, #CatchAndRelease, #FishingDaily, #FishOn, #GoneFishing, #FishingAddict, #FishingTrip, #AnglerLife, #FishingAdventure, #TightLines, #FishingCommunity, #FishingTime, #FreshwaterFishing, #SaltwaterFishing, #BassFishing, #FlyFishing, #FishingLove, #FishingFun, #FishingGear, #FishingSeason, #FishingVibes, #FishOfTheDay"
-    />
-    <meta name="author" content="Albert Cardona" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Fishing With Dummies</title>
-    <link
-      rel="shortcut icon"
-      href="./src/images/logo.png"
-      type="image/x-icon"
-    />
 
-    <script defer src="./src/js/site.js"></script>
+<head>
+  <meta charset="UTF-8" />
+  <meta
+    name="description"
+    content="As Captain Al, I'm getting back into fishing after many years away. I learned to fish with my father as a child, but it's been a long time since then. I'm starting over, picking up tips from various online sources, though none are specific to Spring Hill, Florida. I plan to fish around Clearwater, possibly St. Petersburg, and Tampa. My boat is an old cruiser, not built for fishing, so I'm hoping it won't cause any issues. I'll have company on board and will be trying out online fishing techniques, though they might not always work perfectly for the local waters." />
+  <meta
+    name="keywords"
+    content="#FishingLife, #CatchAndRelease, #FishingDaily, #FishOn, #GoneFishing, #FishingAddict, #FishingTrip, #AnglerLife, #FishingAdventure, #TightLines, #FishingCommunity, #FishingTime, #FreshwaterFishing, #SaltwaterFishing, #BassFishing, #FlyFishing, #FishingLove, #FishingFun, #FishingGear, #FishingSeason, #FishingVibes, #FishOfTheDay" />
+  <meta name="author" content="Albert Cardona" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Fishing With Dummies</title>
+  <link
+    rel="shortcut icon"
+    href="./src/images/logo.png"
+    type="image/x-icon" />
 
-    <link rel="stylesheet" href="./src/css/site.css" />
-  </head>
-  <body>
-    <header data-header>
-      <nav>
-        <div class="logo">
-          <a href="/">
-            <img
-              data-logo
-              id="logo"
-              src="./src/images/boatlogo.png"
-              alt="Logo"
-            />
-            <div class="wave"></div>
-          </a>
-        </div>
-        <div data-toggle-btn class="toggle-links">☰</div>
-        <ul data-nav-links class="nav-links">
-          <li class="link-item"><a href="#hero">Home </a></li>
-          <li class="link-item"><a href="#about">About </a></li>
-          <li class="link-item"><a href="#youtube-links">Videos</a></li>
-        </ul>
-      </nav>
-    </header>
-    <!-- End Header  -->
+  <script defer src="./src/js/site.js"></script>
 
-    <div data-fish class="fish-container">
-      <section id="hero">
-        <h1>Ahoy, Matey!</h1>
-        <img
-          id="mangrove-snapper"
-          src="./src/images/menfish.jpg"
-          alt="I caught a mangrove snapper pic"
-        />
-        <h1>Welcome to Fishing with Dummies!</h1>
-        <div class="paragraph-container">
-          <p>
-            Hello, fellow anglers! I'm Al, your guide on the journey of learning
-            to fish with confidence. As a beginner myself, I understand the
-            challenges and excitement of starting out in the world of fishing.
-            Join me on this adventure as we explore the basics, learn new
-            techniques, and share our experiences. Whether you're a seasoned pro
-            or just dipping your toes into the water, Fishing with Dummies is
-            here to help you cast your line with skill and enjoyment!
-          </p>
-        </div>
-      </section>
-      <!-- End Hero  -->
-      <section id="about">
-        <h1>About Me</h1>
-        <img id="family" src="./src/images/fam.jpg" alt="" />
-        <h1>What I am about</h1>
-        <div class="paragraph-container">
-          <p>
-            My name is Captain AL, a retired Marine with a passion for fishing.
-            This site is an extension of my YouTube channel, Fishing with
-            Dummies, where I share my journey and love for the sport.
-          </p>
-          <p>
-            As a father of seven, fishing has become a cherished hobby,
-            providing both relaxation and excitement. I love my family dearly,
-            and occasionally, you might see my two-year-old son joining me on
-            some of my fishing adventures, adding a special touch to our
-            journeys.
-          </p>
-          <p>
-            Through this platform, I aim to connect with fellow enthusiasts,
-            share tips and experiences, and perhaps even attract sponsorship to
-            enhance my equipment. Whether or not I achieve sponsorship, my true
-            joy lies in fishing and exploring the waters on my boat.
-          </p>
-          <p>Thank you for joining me on this adventure. Tight lines!</p>
-        </div>
-      </section>
-      <!-- End About -->
+  <link rel="stylesheet" href="./src/css/site.css" />
+</head>
 
-      <section id="youtube-links">
-        <h1>Videos</h1>
-        <div data-card-container class="card-container"></div>
-        <!-- end of card container -->
-      </section>
-    </div>
-    <footer>
-      <div class="copy">&copy; FishingWithDummies</div>
-      <span class="youtube-channel">
-        <a target="_blank" href="https://www.youtube.com/@FishingWithDummies78">
-          YouTube Channel
+<body>
+  <header data-header>
+    <nav>
+      <div class="logo">
+        <a href="/">
+          <img
+            data-logo
+            id="logo"
+            src="./src/images/boatlogo.png"
+            alt="Logo" />
+          <div class="wave"></div>
         </a>
-      </span>
-    </footer>
-    <div data-top-button class="goToTop">
-      <span>top</span>
-    </div>
-  </body>
+      </div>
+      <div data-toggle-btn class="toggle-links">☰</div>
+      <ul data-nav-links class="nav-links">
+        <li class="link-item"><a href="#hero">Home </a></li>
+        <li class="link-item"><a href="#about">About </a></li>
+        <li class="link-item"><a href="#youtube-links">Videos</a></li>
+        <li class="link-item"><a href="#footer">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  <!-- End Header  -->
+
+  <div data-fish class="fish-container">
+    <section id="hero">
+      <h1>Ahoy, Matey!</h1>
+      <img
+        id="mangrove-snapper"
+        src="./src/images/menfish.jpg"
+        alt="I caught a mangrove snapper pic" />
+      <h1>Welcome to Fishing with Dummies!</h1>
+      <div class="paragraph-container">
+        <p>
+          Hello, fellow anglers! I'm Al, your guide on the journey of learning
+          to fish with confidence. As a beginner myself, I understand the
+          challenges and excitement of starting out in the world of fishing.
+          Join me on this adventure as we explore the basics, learn new
+          techniques, and share our experiences. Whether you're a seasoned pro
+          or just dipping your toes into the water, Fishing with Dummies is
+          here to help you cast your line with skill and enjoyment!
+        </p>
+      </div>
+    </section>
+    <!-- End Hero  -->
+    <section id="about">
+      <h1>About Me</h1>
+      <img id="family" src="./src/images/fam.jpg" alt="" />
+      <h1>What I am about</h1>
+      <div class="paragraph-container">
+        <p>
+          My name is Captain AL, a retired Marine with a passion for fishing.
+          This site is an extension of my YouTube channel, Fishing with
+          Dummies, where I share my journey and love for the sport.
+        </p>
+        <p>
+          As a father of seven, fishing has become a cherished hobby,
+          providing both relaxation and excitement. I love my family dearly,
+          and occasionally, you might see my two-year-old son joining me on
+          some of my fishing adventures, adding a special touch to our
+          journeys.
+        </p>
+        <p>
+          Through this platform, I aim to connect with fellow enthusiasts,
+          share tips and experiences, and perhaps even attract sponsorship to
+          enhance my equipment. Whether or not I achieve sponsorship, my true
+          joy lies in fishing and exploring the waters on my boat.
+        </p>
+        <p>Thank you for joining me on this adventure. Tight lines!</p>
+      </div>
+    </section>
+    <!-- End About -->
+
+    <section id="youtube-links">
+      <h1>Videos</h1>
+      <div data-card-container class="card-container"></div>
+      <!-- end of card container -->
+    </section>
+  </div>
+  <footer id="footer">
+    <div class="copy">&copy; FishingWithDummies</div>
+    <div class="footer-container">
+      <h2>🎣 Connect with Captain Al</h2>
+      <ul class="footer-links">
+      
+        <li>
+          <a href="https://www.youtube.com/@FishingWithDummies78" target="_blank">
+            <img src="src/images/icons/youtube-logo-2431.svg" alt="YouTube Icon" />
+
+          </a>
+        </li>
+        <li>
+          <a href="https://x.com/fishWitDumm78" target="_blank">
+            <img src="src/images/icons/twitter-x-logo-black-square-20853.svg" alt="X Twitter Icon" />
+
+          </a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@captainalfwd" target="_blank">
+            <img src="src/images/icons/tiktok-logo-4500.svg" alt="TikTok Icon" />
+           
+          </a>
+        </li>
+        <li>
+          <a href="https://www.tumblr.com/fishingwithdummies" target="_blank">
+            <img src="src/images/icons/tumblr-logo-2434.svg" alt="Tumblr Icon" />
+            
+          </a>
+        </li>
+        <li>
+          <a href="https://www.clapperapp.com/fishingwithdummies78" target="_blank">
+            <img src="src/images/icons/Mug_BlackBG.webp" alt="Clapper Icon" />
+          
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com/CaptainALFWD/" target="_blank">
+            <img src="src/images/icons/pinterest-131.svg" alt="Pinterest Icon" />
+             
+          </a>
+        </li>
+      </ul>
+      <div class="icon-pack">
+        Icon by <a class="icon-credit" href='https://iconpacks.net/?utm_source=link-attribution&utm_content=2432'>Iconpacks</a>
+        
+      </div>
+  </footer>
+
+  <div data-top-button class="goToTop">
+    <span>top</span>
+  </div>
+</body>
+
 </html>
